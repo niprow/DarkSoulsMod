17ffc7093a24b3933275c6caa1527ac6b16c1a7b
niprow <prog@nprommi.de>
2020-06-13 11:45:48 +0200

    call registries in registry  events

--- src/main/java/dark_souls_mod/DarkSoulsMod.java
+++ src/main/java/dark_souls_mod/DarkSoulsMod.java
@@ -1,7 +1,11 @@
 package dark_souls_mod;
 
 import dark_souls_mod.block.BlockRegistry;
+import dark_souls_mod.item.ItemRegistry;
+import dark_souls_mod.tileentity.TileEntityTypeRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -39,6 +43,16 @@ public class DarkSoulsMod {
         @SubscribeEvent
         public static void onBlockRegistry(final RegistryEvent.Register<Block> event) {
             event.getRegistry().registerAll(BlockRegistry.getAll());
+        }
+
+        @SubscribeEvent
+        public static void onItemRegistry(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(ItemRegistry.getAll());
+        }
+
+        @SubscribeEvent
+        public static void onTileEntityTypeRegistry(final RegistryEvent.Register<TileEntityType<?>> event) {
+            event.getRegistry().registerAll(TileEntityTypeRegistry.getAll());
         }
     }
 }
