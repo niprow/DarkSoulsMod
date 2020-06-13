ad72625545aeab4b61ef603c0606d1a3823b2b24
niprow <prog@nprommi.de>
2020-06-13 11:51:33 +0200

    bug fixed in TileEntityTypeRegistry

--- src/main/java/dark_souls_mod/tileentity/TileEntityTypeRegistry.java
+++ src/main/java/dark_souls_mod/tileentity/TileEntityTypeRegistry.java
@@ -3,18 +3,16 @@ package dark_souls_mod.tileentity;
 import com.google.common.collect.Lists;
 import dark_souls_mod.Constants;
 import dark_souls_mod.block.BlockRegistry;
-import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.List;
-import java.util.Set;
 
 public class TileEntityTypeRegistry {
     /*-------------------------------- constants --------------------------------*/
     public static final List<TileEntityType<?>> TILE_ENTITY_TYPES = Lists.newArrayList();
-    public static final TileEntityType<BonfireTileEntity> BONFIRE_TILE_ENTITY_TYPE = register(Constants.BONFIRE, new TileEntityType<BonfireTileEntity>(BonfireTileEntity::new, (Set<Block>) BlockRegistry.BONFIRE, null));
+    public static final TileEntityType<BonfireTileEntity> BONFIRE_TILE_ENTITY_TYPE = register(Constants.BONFIRE, TileEntityType.Builder.create(BonfireTileEntity::new, BlockRegistry.BONFIRE).build(null));
 
     /*-------------------------------- automation --------------------------------*/
     public static <T extends TileEntity> TileEntityType<T> register(ResourceLocation tag, TileEntityType<?> tile_entity_type) {
