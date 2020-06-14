8c48e04f91f9601c84401b1ec3071a0d562caf30
niprow <prog@nprommi.de>
2020-06-14 19:15:43 +0200

    full estus remains empty after use

--- src/main/java/dark_souls_mod/item/FullEstusFlask.java
+++ src/main/java/dark_souls_mod/item/FullEstusFlask.java
@@ -1,7 +1,13 @@
 package dark_souls_mod.item;
 
 import dark_souls_mod.item.food.Foods;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class FullEstusFlask extends Item {
     public FullEstusFlask() {
@@ -10,5 +16,16 @@ public class FullEstusFlask extends Item {
                 .containerItem(ItemRegistry.EMPTY_ESTUS_FLASK)
                 .food(Foods.FULL_ESTUS_FLASK)
         );
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack onItemUseFinish(@Nonnull ItemStack stack, @Nonnull World world, @Nonnull LivingEntity living_entity) {
+        if(living_entity instanceof PlayerEntity) {
+            if(!((PlayerEntity) living_entity).inventory.addItemStackToInventory(new ItemStack(ItemRegistry.EMPTY_ESTUS_FLASK))) {
+                ((PlayerEntity) living_entity).dropItem(new ItemStack(ItemRegistry.EMPTY_ESTUS_FLASK), false);
+            }
+        }
+        return super.onItemUseFinish(stack, world, living_entity);
     }
 }
