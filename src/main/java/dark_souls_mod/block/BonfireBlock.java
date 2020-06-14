a583aa2e27dcf1e25c22986cf341e8c7ca47116a
Bastian Neussell <bneussell@yahoo.de>
2020-06-14 16:59:24 +0200

    change bonfire light value to 10

--- src/main/java/dark_souls_mod/block/BonfireBlock.java
+++ src/main/java/dark_souls_mod/block/BonfireBlock.java
@@ -23,7 +23,7 @@ public class BonfireBlock extends Block {
         super(Block.Properties.create(Material.WOOD)
                 .harvestTool(ToolType.AXE)
                 .hardnessAndResistance(4.0f, 4.0f)
-                .lightValue(7)
+                .lightValue(10)
         );
     }
 
